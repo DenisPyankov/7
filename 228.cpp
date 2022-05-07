0dd618eca00c15d3328cb5f8fc4296e9e7026851
Денис Пьянков <91528031+DenisPyankov@users.noreply.github.com>
2022-05-07 12:07:01 +0500

    Add files via upload

---
+++ 228.cpp
@@ -0,0 +1,23 @@
+﻿#include <fstream>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ifstream in("in.txt");
+	ofstream out("out.txt");
+	int n;
+	in >> n;
+	float x = 100;
+	for (int i = 0; i < n; i+=2) {
+		float USD, EUR;
+		in >> USD >> EUR;
+		float newUSD, newEUR;
+		in >> newUSD >> newEUR;
+		x = max( x/USD*newUSD, x/EUR*newEUR);
+	}
+	out << x;
+}
